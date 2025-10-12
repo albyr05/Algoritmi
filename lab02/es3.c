1b82e6c5fbc829f26e9d8515fe2a4646d5e183ee
albyr05 <alberto.reano2@gmail.com>
2025-10-12 13:20:32 +0200

    some changes, now completed

--- lab02/es3.c
+++ lab02/es3.c
@@ -14,11 +14,12 @@ typedef struct {
 
 int alloc_vet(FILE *fp, tratta **v);          
 void ask_input (tratta *v, int *flag, tratta **v_data, tratta **v_code, tratta **v_partenza, tratta **v_arrivo, int nr);
-void freall();
+void freeall(tratta **v, tratta ***v_data, tratta ***v_code, tratta ***v_partenza, tratta ***v_arrivo);
 void print_struct (tratta **v, int nr);
 void alloc_dinamic_struct(tratta *v, tratta ***v_data, tratta ***v_code, tratta ***v_partenza, tratta ***v_arrivo, int nr);
 void merge_sort_pointers(tratta **tipo, int comparison, int l, int r, int nr);
 void merge (tratta **tipo, int comparison, int l, int r, int m, int nr);
+int compare_struct(tratta *a, tratta *b, int par);
 
 int main(){
     FILE *fp = fopen("lab01/es2input.txt", "r");
@@ -29,9 +30,12 @@ int main(){
 
     tratta **v_data, **v_code, **v_partenza, **v_arrivo;
     alloc_dinamic_struct(vp, &v_data, &v_code, &v_partenza, &v_arrivo, nr);
+    fclose(fp);
+    
     while (flag == 0){
         ask_input(vp, &flag, v_data, v_code, v_partenza, v_arrivo, nr);
     }
+    freeall(&vp, &v_data, &v_code, &v_partenza, &v_arrivo);
     
     return 0;
 }
@@ -53,29 +57,39 @@ void ask_input(tratta *v, int *flag, tratta **v_data, tratta **v_code, tratta **
                     case 0:
                         merge_sort_pointers(v_code, input, 0, nr-1, nr);
                         print_struct(v_code, nr);
+                        break;
                     case 1:
                         merge_sort_pointers(v_partenza, input, 0, nr-1, nr);
                         print_struct(v_partenza, nr);
+                        break;
                     case 2:
                         merge_sort_pointers(v_arrivo, input, 0, nr-1, nr);
                         print_struct(v_arrivo, nr);
+                        break;
                     case 3:
-                          merge_sort_pointers(v_data, input, 0, nr-1, nr);
-                          print_struct(v_data, nr);
-                    break;
+                        merge_sort_pointers(v_data, input, 0, nr-1, nr);
+                        print_struct(v_data, nr);
+                        break;
                 }
             break;
         
         case 1:
             printf("Inserisci il nome del file in input: ");
-            scanf ("%s", filename);
-            sprintf(path, "/..%s", filename);
+            scanf("%s", filename);
+            sprintf(path, "lab02/%s", filename);
             FILE *newf = fopen(path, "r");
-            if (newf == NULL) return;
+            if (newf == NULL) {
+                printf("Errore: file non trovato\n");
+                return;
+            }
 
-            //freeall(vp);
-            nr = alloc_vet (newf, v);
+            freeall(&v, &v_data, &v_code, &v_partenza, &v_arrivo);
             
+            nr = alloc_vet(newf, &v);
+            alloc_dinamic_struct(v, &v_data, &v_code, &v_partenza, &v_arrivo, nr);
+            fclose(newf);
+            
+            // NON chiamare ask_input() qui - lascia che il loop principale continui
             break;
 
         case 2:
@@ -133,22 +147,21 @@ void merge_sort_pointers(tratta **tipo, int comparison, int l, int r, int nr){
 }
 
 int compare_struct(tratta *a, tratta *b, int par){
-    if (par == 0) {
-        if (strcmp(a->data, b->data) == 0){
-            return strcmp(a->ora_p, b->ora_p);
-        }
-        return strcmp(a->data, b->data);
+    if (par == 0) return strcmp(a->code, b->code);
+    else if (par == 1) return strcmp(a->partenza, b->partenza);
+    else if (par == 2) return strcmp(a->destinazione, b->destinazione);
+    else if (par == 3) {
+        int cmp = strcmp(a->data, b->data);
+        if (cmp == 0) return strcmp(a->ora_p, b->ora_p);
+        return cmp;
     }
-    else if (par == 1) return strcmp(a->code, b->code);
-    else if (par == 2) return strcmp (a->partenza, b->partenza);
-    else if (par == 3) return strcmp (a->destinazione, b->destinazione);
     return 0;
 }
 
 
 void merge(tratta **tipo, int comparison, int l, int r, int m, int nr){
     tratta *supp[nr];
-    int i = l; int j = m+1, k = l;
+    int i = l, j = m+1, k = l;
     while (i <= m && j <= r){
         if (compare_struct(tipo[i], tipo[j], comparison) <= 0){
             supp[k] = tipo[i++];
@@ -164,7 +177,20 @@ void merge(tratta **tipo, int comparison, int l, int r, int m, int nr){
     while (j <= r){
         supp[k++] = tipo[j++];
     }
-    for (i = l; i < nr; i++){
+    for (i = l; i <= r; i++){
         tipo[i] = supp[i];
     }
+}
+
+void freeall(tratta **v, tratta ***v_data, tratta ***v_code, tratta ***v_partenza, tratta ***v_arrivo){
+    free(*v);
+    free(*v_data);
+    free(*v_code);
+    free(*v_partenza);
+    free(*v_arrivo);
+    *v = NULL;
+    *v_data = NULL;
+    *v_code = NULL;
+    *v_partenza = NULL;
+    *v_arrivo = NULL;
 }
