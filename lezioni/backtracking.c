c32b943917c020940b6139448e121f7fa2dfad0e
albyr05 <alberto.reano2@gmail.com>
2025-10-26 18:17:10 +0100

    h

--- lezioni/backtracking.c
+++ lezioni/backtracking.c
@@ -21,6 +21,42 @@ int disp_semplici(int pos, int *val, int *mark, int *sol, int cnt, int n, int k)
     return cnt;
 }
 
+int disp_rip(int pos, int *val, int *sol, int cnt, int n, int k){
+    int i;
+    if (pos >= k){
+        for (i = 0; i < k; i ++){
+            printf("%d", sol[i]);
+        }
+        printf("\n");
+        return cnt +1;
+    }
+    for (i = 0; i<n; i++){
+        sol[pos] = val[i];
+        cnt = disp_rip(pos+1, val, sol, cnt, n, k);
+    }
+    return cnt;
+}
+
+int perm(int pos, int *val, int *sol, int* mark,  int cnt, int n){
+    int i;
+    if (pos >= n){
+        for (i = 0; i<n; i++){
+            printf("%d", sol[i]);
+        }
+        printf("\n");
+        return cnt +1;
+    }
+    for (i = 0; i < n; i++){
+        if (mark[i] == 0){
+            mark[i] = 1;
+            sol[pos] = val[i];
+            cnt = perm(pos+1, val, sol, mark, cnt, n);
+            mark[i] = 0;
+        }
+    }
+    return cnt;
+}
+
 int main(){
     int n = 4, k = 2;
     int *val = malloc(n*sizeof(int));
@@ -30,9 +66,19 @@ int main(){
     int *mark = calloc(n, sizeof(int));
     int *sol = malloc(k*sizeof(int));
     int cnt = 0, pos = 0;
+
+    // disp semplici
     cnt = disp_semplici(pos, val, mark, sol, cnt, n, k);
-    printf("\nCi sono %d disposizioni semplici di %d elementi in classe %d", cnt, n, k);
+    printf("\nCi sono %d disposizioni semplici di %d elementi in classe %d\n", cnt, n, k);
+    
+    // disp rip
+    cnt = disp_rip(pos, val, sol, 0, n, k);
+    printf("\nCi sono %d disposizioni con ripetizioni di %d elementi in classe %d\n", cnt, n, k);
 
-
+    // perm semplici
+    free(sol);
+    sol = malloc(n*sizeof(int));
+    cnt = perm(pos, val, sol, mark, 0, n);
+    printf("ci sono %d permutazioni di %d elementi", cnt, n);
     return 0;
 }
